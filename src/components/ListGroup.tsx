7e33d010875021db8e51164ace6fd00b7c47f4eb
Hosama Adem <hosamaadem8@gmail.com>
2025-08-19 19:52:47 +0300

    the second commit mostly styling and hover by bootstrap

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,23 +1,23 @@
-
+import {MouseEvent} from "react";
 
 function ListGroup() {
-  let items=[
-    'New York',
-    'sanfransico',
-    'Tokyo',
-    'London',
-    'Paris'
-  ];
-  items=[];
-
+  let items=['AddisAbaba','Tokyo','Istanbul','London','Paris'];
+  
+  let selectedIndex=0;
+  //event handler 
+  const handleClick=(event:MouseEvent)=>console.log(event)
 
   return (
     <>
         <h1>List</h1>
         {items.length===0 && <p>No item found</p>}
         <ul className="list-group">
-          {items.map((item) =>(
-            <li key={item}>{item}</li>
+          {items.map((item,index) =>(
+            <li className={selectedIndex === index ? 'list-group-item active': 'list-group-item'}
+            key={item} 
+            onClick={handleClick}>
+                {item}
+                </li>
           ))}
         </ul>
     </>
